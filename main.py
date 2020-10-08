a5930370a0db05270725140b1ae37aa2cba9f1b4
val8119 <48407846+val8119@users.noreply.github.com>
2020-10-08 23:56:20 +0300

    Added "New File" functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
+import tkinter.ttk
 
 # variables
 file_types = [("All types (*.*)", "*.*"), ("Text file (*.txt)", "*.txt"), ("Python (*.py)", "*.py")]
@@ -17,7 +18,11 @@ def about():
     tk.messagebox.showinfo("About ShitDE v0.1", "What is ShitDE:\nShitDE is something I made because I was bored. I was supposed to play Among Us! with friends but only like 5 people showed up so we got bored and I ended up doing this.\n\nWhat does ShitDE mean:\nShitDE means Shit Development Environment, because I can't be asked to come up with a name for something as shitty as this.\n\nMade by val8119 on GitHub")
 
 
-def file_save(event):
+def ctrl_s(event):
+    file_save()
+
+
+def file_save():
     f = tk.filedialog.asksaveasfile(mode='w', defaultextension="*.txt", filetypes=file_types)
     if f is None:
         return
@@ -34,24 +39,39 @@ def file_open():
     text_area.insert(1.0, open(f, 'r').read())
 
 
+def ask_save():
+    ask_dialog = tk.messagebox.askquestion("New File", "Save current file?",icon="warning")
+
+    if ask_dialog == "yes":
+        file_save()
+        text_area.delete(1.0, "end")
+    else:
+        text_area.delete(1.0, "end")
+
+
 # menu bar
 menu = tk.Menu(root)
 filemenu = tk.Menu(menu, tearoff=0)
-filemenu.add_command(label="New file", command=do_nothing)
-filemenu.add_command(label="Open file...", command=file_open)
-filemenu.add_command(label="Save as...", command=file_save)
+filemenu.add_command(label="New File", command=ask_save)
+filemenu.add_command(label="Open File...", command=file_open)
+filemenu.add_command(label="Save As...", command=file_save)
 menu.add_cascade(label="File", menu=filemenu)
 
 helpmenu = tk.Menu(menu, tearoff=0)
 helpmenu.add_command(label="About...", command=about)
 menu.add_cascade(label="Help", menu=helpmenu)
 
-text_area = tk.Text(fg="#ededed", font=("Courier New", 10, "bold"))
+# text area
+text_container = tk.Frame(root, borderwidth=0)
+
+text_area = tk.Text(fg="#ededed", font=("Courier New", 10, "bold"), borderwidth=0, wrap="no")
 text_area.pack(expand="yes", fill="both")
+
+
 
 root.geometry("600x400")
 root.config(menu=menu)
-root.bind("<Control-s>", file_save)
+root.bind("<Control-s>", ctrl_s)
 root.iconphoto(False, tk.PhotoImage(file="icon.png"))
 root.title("ShitDE v0.1")
 root.mainloop()
